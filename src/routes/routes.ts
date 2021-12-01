c47a2c25405ac5d7a17661d085a562d2bb2a9450
ArianAngoma <arian.angoma@tecsup.edu.pe>
2021-12-01 08:08:31 -0500

    Rutas independientes

---
+++ src/routes/routes.ts
@@ -0,0 +1,30 @@
+/* Importaciones propias */
+import {LazyPage1, LazyPage2, LazyPage3} from '../01-LazyLoad/pages';
+
+type JSXComponent = () => JSX.Element;
+
+/* Interfaz para las rutas */
+interface Route {
+    path: string;
+    Component: JSXComponent;
+    name: string;
+    children?: Route[];
+}
+
+export const routes: Route[] = [
+    {
+        path: '/lazy1',
+        Component: LazyPage1,
+        name: 'LazyPage-1'
+    },
+    {
+        path: '/lazy2',
+        Component: LazyPage2,
+        name: 'LazyPage-2'
+    },
+    {
+        path: '/lazy3',
+        Component: LazyPage3,
+        name: 'LazyPage-3'
+    }
+]
