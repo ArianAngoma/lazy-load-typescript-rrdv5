15dfebf9768af22820122307e2ece84e04134fbc
ArianAngoma <arian.angoma@tecsup.edu.pe>
2021-12-03 06:49:12 -0500

    Revertir cambios en la rama main

--- src/routes/Navigations.tsx
+++ src/routes/Navigations.tsx
@@ -7,8 +7,6 @@ import {
 
 /* Importaciones propias */
 import logo from '../logo.svg';
-import {LazyPage2, LazyPage3} from '../01-LazyLoad/pages';
-import {ShoppingPage} from '../02-component-patterns/pages/ShoppingPage';
 
 export const Navigations = () => {
     return (
@@ -16,32 +14,29 @@ export const Navigations = () => {
             <div className="main-layout">
                 <nav>
                     <img src={logo} alt="React Logo"/>
-
                     <ul>
                         <li>
-                            <NavLink to="/shopping" activeClassName="nav-active" exact>Shopping</NavLink>
+                            <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/lazy2" activeClassName="nav-active" exact>Lazy2</NavLink>
+                            <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/lazy3" activeClassName="nav-active" exact>Lazy3</NavLink>
+                            <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
                         </li>
                     </ul>
                 </nav>
 
                 <Switch>
-
-                    <Route path="/shopping">
-                        <ShoppingPage/>
+                    <Route path="/about">
+                        <h1>About</h1>
                     </Route>
-                    <Route path="/lazy2">
-                        <LazyPage2/>
+                    <Route path="/users">
+                        <h1>Users</h1>
                     </Route>
-                    <Route path="/lazy3">
-                        <LazyPage3/>
+                    <Route path="/">
+                        <h1>Home</h1>
                     </Route>
-
                 </Switch>
             </div>
         </Router>
