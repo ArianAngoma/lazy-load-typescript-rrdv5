d892416a8d2523bd44744ee079de5e666d03c8dc
ArianAngoma <arian.angoma@tecsup.edu.pe>
2021-12-04 15:40:45 -0500

    Mostrar valor inicial en el componente

--- src/02-component-patterns/hooks/useProduct.tsx
+++ src/02-component-patterns/hooks/useProduct.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 /* Importaciones propias */
 import {InitialValues, onChangeArgs, Product} from '../interfaces/interfaces';
@@ -13,6 +13,9 @@ interface useProductArgs {
 export const useProduct = ({product, onChange, value = 0, initialValues}: useProductArgs) => {
     const [counter, setCounter] = useState<number>(initialValues?.count || value);
 
+    /* Referencia si el componente esta montado */
+    const isMounted = useRef(false);
+
     const increaseBy = (value: number) => {
         const newValue = Math.max(counter + value, 0);
         setCounter(newValue);
@@ -23,8 +26,13 @@ export const useProduct = ({product, onChange, value = 0, initialValues}: usePro
 
     /* Cambiar el estado si el value cambia */
     useEffect(() => {
+        if (!isMounted.current) return;
         setCounter(value)
     }, [value]);
+
+    useEffect(() => {
+        isMounted.current = true;
+    }, []);
 
     return {counter, increaseBy}
 }
