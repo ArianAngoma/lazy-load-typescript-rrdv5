7640cb716190d09113c84668f9504666142a8617
ArianAngoma <arian.angoma@tecsup.edu.pe>
2021-12-06 14:28:06 -0500

    Obtener datos del formulario

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -1,23 +1,44 @@
+import {useFormik} from 'formik';
+
+/* Importaciones propias */
 import '../styles/styles.css';
 
 export const FormikBasicPage = () => {
+    const {handleChange, values, handleSubmit} = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: ''
+        },
+        onSubmit: async values => {
+            console.log(values);
+        }
+    });
+
     return (
         <div>
             <h1>Formik Basic Tutorial</h1>
-            <form noValidate>
+            <form noValidate
+                  onSubmit={handleSubmit}>
                 <label htmlFor="firstName">First Name</label>
                 <input type="text"
-                       name="firstName"/>
+                       name="firstName"
+                       onChange={handleChange}
+                       value={values.firstName}/>
                 <span>First name is required</span>
 
                 <label htmlFor="lastName">Last Name</label>
                 <input type="text"
-                       name="lastName"/>
+                       name="lastName"
+                       onChange={handleChange}
+                       value={values.lastName}/>
                 <span>Last name is required</span>
 
                 <label htmlFor="email">Email Address</label>
                 <input type="email"
-                       name="email"/>
+                       name="email"
+                       onChange={handleChange}
+                       value={values.email}/>
                 <span>Email is required</span>
                 <span>Check for an valid email format</span>
 
